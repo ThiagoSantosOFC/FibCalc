ae2e2e4707dbc178a2ec611ff02a26fe6f8311e7
Thiago Santos <t.mateus.santos2004@gmail.com>
2022-06-30 03:44:03 +0100

    Update fib.js

--- fib.js
+++ fib.js
@@ -6,7 +6,4 @@ function fib(n) {
     return fib(n - 1) + fib(n - 2);
     }
     console.log(fib(10));
-    console.log(fib(20));
-    console.log(fib(30));
-    console.log(fib(40));
-    console.log(fib(50));
+ 
